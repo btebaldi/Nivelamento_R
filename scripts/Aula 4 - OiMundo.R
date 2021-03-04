a5bffe77ac47a91456af5109d38933da0468d26f
Teo <btebaldi@deskpaper.com>
2021-03-04 15:02:53 -0300

    aula 4

---
+++ scripts/Aula 4 - OiMundo.R
@@ -0,0 +1,8 @@
+rm(list = ls())
+frase <- sprintf("%s\nOi mundo\n%s\n",
+                 paste(rep("*", 20), collapse = ""),
+                 paste(rep("*", 20), collapse = ""))
+cat(frase)
+frase <- sprintf("Imprimindo base de dados: Harman Example 2.3\n")
+cat(frase)
+print(Harman23.cor)
