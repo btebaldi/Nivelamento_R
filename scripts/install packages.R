440af939822a6734c774659625b91adfe265c54d
Teo <btebaldi@deskpaper.com>
2021-07-15 12:53:35 -0300

    Pacotes mais utilizados

--- scripts/install packages.R
+++ scripts/install packages.R
@@ -1,55 +1,68 @@
 rm(list = ls())
 
-pck <- c("astsa", 
-         "bizdays", 
-         "broom", 
-         "caret", 
-         "cowplot", 
-         "data.table", 
-         "dlm", 
-         "dplyr", 
-         "e1071", 
-         "elasticnet", 
-         "fmsb", 
-         "forcats", 
-         "ggplot2", 
-         "glmnet", 
-         "lmtest", 
-         "lubridate", 
-         "magrittr", 
-         "MASS", 
-         "MTS", 
-         "mvtnorm", 
-         "forecast", 
-         "nlme", 
-         "plm", 
-         "randomForest", 
-         "RColorBrewer", 
-         "readr", 
-         "readxl", 
-         "rstudioapi", 
-         "rugarch", 
-         "sandwich", 
-         "tibble", 
-         "tidyr", 
-         "timeSeries", 
-         "TSA", 
-         "tsDyn", 
-         "tseries", 
-         "tseriesChaos", 
-         "urca", 
-         "vars", 
-         "wooldridge", 
-         "xgboost", 
-         "xts", 
-         "zoo",
-         "sidrar",
-         "stringi",
-         "stringr")
+pck_list <- c(
+  # Must have
+  "dplyr",        # Analise de banco de dados
+  "ggplot2",      # Pacote para graficos
+  "readr",        # Carrega base de dados em arquivos texto (CSV, etc.)
+  "readxl",       # Carrega base de dados em arquivos excel (Xlsx, xls)
+  "tibble",       # Pacote de tabelas mais esquematizadas
+  "stringi",      # Pacotes de trabalho de Strings
+  "stringr",      # Pacotes de trabalho de Strings
+  "magrittr",     # Pipe %>% 
+  "lubridate",    # gerenciamento de datas
+  "tidyr",        # Analise de banco de dados (Must have)
+  
+  # Muito bom
+  "plm",          # Pacote de regressoes em painel
+  "AER",          # Applied Econometrics with R
+  "urca",         # Analise de modelos VAR e VECM (muito bom!)
+  "vars",         # Analise de modelos VAR e VECM (muito bom!)
+  "cowplot",      # Pacore para ajuste de Gaficos
+  "rvest",        # Web Scrapping
+  
+  # Desejavel
+  "zoo",          # Pacote base para Time Series
+  "glmnet",       # Utilizado para Lasso and Elastic-Net Regularized Generalized
+  "tseries", 
+  "timeSeries",   # Analise de Serie de tempo
+  "TSA",          # Analise de Serie de tempo (Muda o ACF)
+  "tsDyn",        # Analise de modelos VAR e VECM (muito bom!)
+  "forcats",      # Tools for Working with Categorical Variables (Factors)
+  "mvtnorm",      # Normal Multivariada
+  "forecast",     # Forecasting Functions for Time Series and Linear Models
+  "bizdays",      # Pacote para analise de dias uteis
+  "data.table",   # Analise de banco de dados
+  "xts", 
+  
+  # Utilizado em aplicacoes especificas
+  "sandwich", 
+  "rugarch",      # Modelagem da variancia. Pacotes Garch e afins
+  "sidrar",       # Pacotes de acesso a dados do SIDRA
+  "wooldridge",   # Banco de dados do wooldridge
+  "xgboost",      # Pacote para analie de regressoes de XGBoost
+  "e1071",        # Misc Functions of of Statistics (Support Vector Machines)
+  "randomForest", # Regressoes de Floresta aleatoria
+  "progress",     # barra de progresso
+  "jsonlite",     # interpretação de arquivos JSON
+  "astsa", 
+  "broom", 
+  "caret",        # Pacote para utilizacao de varios pacotes de Machine Learning
+  "dlm",          # Dynamic Linear Models
+  "RColorBrewer", # Pacote para escolha de cores
+  "fmsb", 
+  "lmtest",       # testes Econometricos
+  "MASS", 
+  "MTS", 
+  "nlme", 
+  "rstudioapi", 
+  "tseriesChaos", 
+)
+
 
 x <- readline("Instalar pacotes? [Yes/No]")
-if(x == "Yes"){
+if(x %in% c("Yes", "yes")){
   cat(sprintf("\nInstalando os pacotes.\n"))
-  install.packages(pck)
+  install.packages(pck_list)
 }
 
